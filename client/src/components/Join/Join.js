1782157385a352856d4138c9ce22804c02a0dd09
Emjacks <emjackson107@gmail.com>
2023-03-01 17:01:31 -0500

    second change client

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -9,19 +9,23 @@ export default function SignIn() {
   const [room, setRoom] = useState('');
 
   return (
+    <div>
+      <div className='header'>
+      <h1>Welcome to Chatprivacy</h1>
+      <p>Unlimited amount of people can join your room. Keeping your messages private.</p>
+      </div>
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
-        <h1 className="heading">Create/join a room</h1>
-        <div>
+        <h1 className="heading">Create or Join a room</h1>
+        <div className='form-container'>
           <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
-        </div>
-        <div>
           <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
         </div>
         <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
           <button className={'button mt-20'} type="submit">Sign In</button>
         </Link>
       </div>
+    </div>
     </div>
   );
 }
